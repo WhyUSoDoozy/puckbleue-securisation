01c08fd200f96b1cda0f143ca2f3e9f53a4222de
arqoz. <112104000+WhyUSoDoozy@users.noreply.github.com>
2025-04-08 22:27:27 -0400

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+-SOURCE_DIR="/var/www"
+BACKUP_DIR="/var/backup"
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+BACKUP_FILE="$BACKUP_DIR/www_backup_$TIMESTAMP.tar.gz"
+
+-mkdir -p "$BACKUP_DIR"
+
+-tar -czf "$BACKUP_FILE" "$SOURCE_DIR"
+
+-echo "Backup of $SOURCE_DIR completed: $BACKUP_FILE"
